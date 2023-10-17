22eccb88b279c3129c3694713865dee790a35f5b
Oleg <oleg@oleg.oleg>
2023-10-17 13:10:00 +0200

    ch ch ch changes

--- DAM2_M07_PR05_PANELL_COLORS_WPF/MainWindow.xaml.cs
+++ DAM2_M07_PR05_PANELL_COLORS_WPF/MainWindow.xaml.cs
@@ -23,36 +23,90 @@ namespace DAM2_M07_PR05_PANELL_COLORS_WPF
         public MainWindow()
         {
             InitializeComponent();
+            colorPicker.SelectedColorChanged += ColorPicker_SelectedColorChanged;
         }
-
-        private void IntegerUpDown_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
-
+            if (e.NewValue.HasValue)
+            {
+                Color selectedColor = e.NewValue.Value;
+                SolidColorBrush brush = new SolidColorBrush(selectedColor);
+                cvColors.Background = brush; // Aplica el color al fondo del Canvas
+            }
         }
 
         private void scbNumeroA_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            if (sldrA != null)
+            {
+                scbNumeroA.Value = sldrA.Value;
 
+            }
+            //double newValue = e.NewValue;
+
+            //// Sincronizar con R, G y B
+            //scbNumeroR.Value = newValue;
+            //scbNumeroG.Value = newValue;
+            //scbNumeroB.Value = newValue;
+
+            // Aquí puedes actualizar el fondo del Canvas o realizar otras acciones según sea necesario
+            UpdateCanvasColor();
         }
 
         private void scbNumeroR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            double newValue = e.NewValue;
 
+            // Sincronizar con A, G y B
+            scbNumeroA.Value = newValue;
+            scbNumeroG.Value = newValue;
+            scbNumeroB.Value = newValue;
+
+            // Aquí puedes actualizar el fondo del Canvas o realizar otras acciones según sea necesario
+            UpdateCanvasColor();
         }
 
         private void scbNumeroG_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            double newValue = e.NewValue;
 
+            // Sincronizar con A, R y B
+            scbNumeroA.Value = newValue;
+            scbNumeroR.Value = newValue;
+            scbNumeroB.Value = newValue;
+
+            // Aquí puedes actualizar el fondo del Canvas o realizar otras acciones según sea necesario
+            UpdateCanvasColor();
         }
 
         private void scbNumeroB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            double newValue = e.NewValue;
 
+            // Sincronizar con A, R y G
+            scbNumeroA.Value = newValue;
+            scbNumeroR.Value = newValue;
+            scbNumeroG.Value = newValue;
+
+            // Aquí puedes actualizar el fondo del Canvas o realizar otras acciones según sea necesario
+            UpdateCanvasColor();
         }
 
-        private void colorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        private void UpdateCanvasColor()
         {
+            // Obtener los valores A, R, G y B de los controladores
+            byte alpha = (byte)scbNumeroA.Value;
+            byte red = (byte)scbNumeroR.Value;
+            byte green = (byte)scbNumeroG.Value;
+            byte blue = (byte)scbNumeroB.Value;
 
+            // Crear un nuevo SolidColorBrush y aplicarlo al fondo del Canvas
+            Color color = Color.FromArgb(alpha, red, green, blue);
+            SolidColorBrush brush = new SolidColorBrush(color);
+            cvColors.Background = brush;
         }
+
+
+
     }
 }
